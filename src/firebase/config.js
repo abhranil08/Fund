d8672412274cfbd51ba5225e7ffbfbec20ba3ac8
ninad99 <ninuch7@gmail.com>
2020-01-06 00:20:05 +0530

    Added environment variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,18 +3,16 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: process.env.FUNDTAX_APP_apiKey,
-  authDomain: process.env.FUNDTAX_APP_authDomain,
-  databaseURL: process.env.FUNDTAX_APP_databaseURL,
-  projectId: process.env.FUNDTAX_APP_projectId,
-  storageBucket: process.env.FUNDTAX_APP_storageBucket,
-  messagingSenderId: process.env.FUNDTAX_APP_messagingSenderId,
-  appId: process.env.FUNDTAX_APP_appId,
-  measurementId: process.env.FUNDTAX_APP_measurementId
+  apiKey: process.env.REACT_APP_FUNDTAX_apiKey,
+  authDomain: process.env.REACT_APP_FUNDTAX_authDomain,
+  databaseURL: process.env.REACT_APP_FUNDTAX_databaseURL,
+  projectId: process.env.REACT_APP_FUNDTAX_projectId,
+  storageBucket: process.env.REACT_APP_FUNDTAX_storageBucket,
+  messagingSenderId: process.env.REACT_APP_FUNDTAX_messagingSenderId,
+  appId: process.env.REACT_APP_FUNDTAX_appId,
+  measurementId: process.env.REACT_APP_FUNDTAX_measurementId
 };
 
 firebase.initializeApp(firebaseConfig);
-
-export const database = firebase.database();
 
 export default firebase;
