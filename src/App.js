6fe88f87f72ae46fee239318dd667e198836026a
ninad99 <ninuch7@gmail.com>
2020-01-06 00:20:57 +0530

    Added authentication state and signout logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import classes from './App.module.css';
 
@@ -8,14 +8,37 @@ import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
 
+import firebase from './firebase/config';
+
 const App = props => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userID, setUserID] = useState(null);
+
+  const storeUser = (userID) => {
+    setUserID(userID);
+    setIsAuthenticated(true);
+    alert('Signed in successfully!');
+    console.log(userID);
+  };
+
+  const handleSignOut = useCallback(async () => {
+    try {
+      await firebase.auth().signOut();
+    } catch (err) {
+      console.log(err);
+    }
+    alert('Signed out successfully!');
+    setIsAuthenticated(false);
+    setUserID(null);
+  }, []);
+
   return (
     <main className={classes.App}>
-      <Navigation />
+      <Navigation isAuthenticated={isAuthenticated} userID={userID} handleSignOut={handleSignOut} />
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/login" render={(props) => <Login {...props} />} />
-        <Route path="/signup" render={(props) => <Signup {...props} />} />
+        <Route path="/login" render={(props) => <Login {...props} storeUser={storeUser} />} />
+        <Route path="/signup" render={(props) => <Signup {...props} storeUser={storeUser} />} />
       </Switch>
       <Footer />
     </main>
