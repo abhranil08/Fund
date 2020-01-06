3bbe79a5519d38eabc424c594a8eb4280528376d
ninad99 <ninuch7@gmail.com>
2020-01-06 16:43:22 +0530

    Added database write on signup + loading styles

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -3,7 +3,7 @@ import { Link, withRouter } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 
-import firebase from '../../firebase/config';
+import firebase, { database } from '../../firebase/config';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -12,6 +12,7 @@ import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
+import Spinner from 'react-bootstrap/Spinner';
 import classes from './Signup.module.css';
 
 const isValid = (email, passwd) => {
@@ -21,27 +22,41 @@ const isValid = (email, passwd) => {
   return validEmail && validPasswd;
 }
 
+const writeUserData = (userId, userName, email) => {
+  database.ref('users/' + userId).set({
+    username: userName,
+    email: email
+  });
+}
+
 const Signup = props => {
   const { history, storeUser } = props;
+  const [inputUsername, setInputUsername] = useState('');
   const [inputEmail, setInputEmail] = useState('');
   const [inputPassword, setInputPassword] = useState('');
   const [hiddenPassword, setHiddenPassword] = useState(true);
   const [formError, setFormError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const formSubmit = () => {
+    setIsLoading(true);
     if (isValid(inputEmail, inputPassword)) {
       setFormError(false);
-      handleSignUp(inputEmail, inputPassword);
+      handleSignUp(inputEmail, inputPassword, inputUsername);
     } else {
       console.log('Invalid username or password!');
       setFormError(true);
+      setIsLoading(false);
     }
   }
 
-  const handleSignUp = useCallback(async (email, password) => {
+  const handleSignUp = useCallback(async (email, password, userName) => {
     try {
       const userCredentials = await firebase.auth().createUserWithEmailAndPassword(email, password);
-      storeUser(userCredentials.uid);
+      // console.log(userCredentials.user.uid);
+      writeUserData(userCredentials.user.uid, userName, email);
+      storeUser(userCredentials.user.uid, userName);
+      setIsLoading(false);
       history.push('/');
     } catch (err) {
       console.log(err);
@@ -56,6 +71,13 @@ const Signup = props => {
             <Card className={classes.signupCard} body>
               <Form>
                 {formError && <Alert variant="danger">Invalid email or password!</Alert>}
+                <Form.Group controlId="formBasicName">
+                  <Form.Label>Username</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter username"
+                    onChange={event => setInputUsername(event.target.value)} />
+                </Form.Group>
                 <Form.Group controlId="formBasicEmail">
                   <Form.Label>Email address</Form.Label>
                   <Form.Control
@@ -74,7 +96,10 @@ const Signup = props => {
                   <Form.Check type="checkbox" label="Show Password" onChange={event => setHiddenPassword(!hiddenPassword)} />
                 </Form.Group>
                 <Button className={classes.signupButton} variant="primary" onClick={formSubmit}>
-                  Sign Up
+                  Sign Up&nbsp;&nbsp;
+                  {isLoading && <Spinner animation="grow" role="status">
+                    <span className="sr-only">Loading...</span>
+                  </Spinner>}
                 </Button>
                 <div className="mt-3">
                   Already have an account? <Link to="/login">Login</Link>
