cceed511a29e3325fbb8243206f659c09dbfe70d
ninad99 <ninuch7@gmail.com>
2020-01-20 23:31:17 +0530

    Line chart added

---
+++ src/components/LineChart/LineChart.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+
+const LineChart = props => {
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    datasets: [
+      {
+        label: 'Example dataset',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40, 45, 47, 50, 52, 49]
+      }
+    ]
+  };
+
+  return (
+    <Line data={data} legend={{ position: 'right' }}/>
+  );
+}
+
+export default LineChart;
